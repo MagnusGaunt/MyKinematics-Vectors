fe04a2b3fb491d588e02db9a789d71812cc18386
magnus <51099873+MagnusGaunt@users.noreply.github.com>
2025-08-06 23:43:36 -0400

    full thing done I think

--- .src/kinematicsVectors.h
+++ .src/kinematicsVectors.h
@@ -1,5 +1,63 @@
-#ifdef KINEMATICS_VECTORS_H
+#ifndef KINEMATICS_VECTORS_H
 #define KINEMATICS_VECTORS_H
 
+#include <vector>
 
-#endif
+class KVector{
+public:
+    KVector(size_t columns, size_t rows = 1) : m_vectors(columns, std::vector<float>(rows)) {}
+
+    KVector(KVector& copy) : m_vectors{}{ *this = copy;};
+
+    KVector(std::vector<std::vector<float>> input) : m_vectors {}{
+        m_vectors = input;
+    }
+
+    void operator=(const std::vector<std::vector<float>> input);
+    void operator=(const KVector& v);
+    void operator+=(const KVector& v);
+    void operator-=(const KVector& v);
+    void operator*=(const KVector& v);
+    void operator/=(const KVector& v);
+    KVector operator+(const KVector& v);
+    KVector operator-(const KVector& v);
+    KVector operator*(const KVector& v);
+    KVector operator/(const KVector& v);
+
+    KVector operator+(const std::vector<float>& v);
+    KVector operator-(const std::vector<float>& v);
+    KVector operator*(const std::vector<float>& v);
+    KVector operator/(const std::vector<float>& v);
+
+    KVector operator+(double add);
+    KVector operator-(double sub);
+    KVector operator*(double scale);
+    KVector operator/(double dev);
+
+    KVector setConstant(double value);
+    KVector setConstant(std::vector<double>& vector);
+
+    const double magnitude(int index);
+    const double maxMagnitude();
+    const double avgMagnitude();
+
+    const std::vector<float> getRow(int i) const {return m_vectors[i];};
+    constexpr int getRows() const { return m_vectors.size();};
+    constexpr int getColumns() const { return m_vectors[0].size();};
+
+    std::vector<std::vector<float>> getVector() {return m_vectors;};
+
+private:
+    template <typename Op>
+    void applyKVectorOperation(const std::vector<std::vector<float>>& v, Op op);
+
+    template <typename Op>
+    void applyVectorOperation(const std::vector<float>& v, Op op);
+
+    template <typename T, typename Op>
+    void applyScalarOperation(const T num, Op op);
+
+    std::vector<std::vector<float>> m_vectors;
+};
+
+#endif // KINEMATICS_VECTORS_H
--- .src/main.cpp
+++ .src/main.cpp
@@ -1,1 +1,190 @@
-#include "kinematicsVectors"
+#include "kinematicsVectors.h"
+#include <vector>
+#include <cmath>
+
+namespace {
+    auto add_op = [](double& a, double b) { a += b; };
+    auto sub_op = [](double& a, double b) { a -= b; };
+    auto mult_op = [](double& a, double b) { a *= b; };
+    auto dev_op = [](double& a, double b) {a /= b; };
+    auto set_op = [](double& a, double b) { a = b; };
+}
+
+template <typename Op>
+void KVector::applyKVectorOperation(const std::vector<std::vector<float>>& v, Op op){
+    for (int i = 0; i < m_vectors.size() && i < v.size(); ++i){
+        for (int j = 0; j < m_vectors[0].size() && j < v[0].size(); ++j){
+            op(m_vectors[i][j], v[i][j]);
+        }
+    }
+}
+
+template <typename Op>
+void KVector::applyVectorOperation(const std::vector<float>& v, Op op){
+    for (int i = 0; i < m_vectors.size(); ++i){
+        for (int j = 0; j < m_vectors[0].size() && j < v.size(); ++j){
+            op(m_vectors[i][j], v[j]);
+        }
+    }
+}
+
+template <typename T, typename Op>
+void KVector::applyScalarOperation(const T num, Op op){
+    for (int i = 0; i < m_vectors.size(); ++i){
+        for (int j = 0; j < m_vectors[0].size() ++j){
+            op(m_vectors[i][j], num);
+        }
+    }
+}
+
+//////////////////////////////////////////////////////////
+
+void KVector::operator=(const std::vector<std::vector<float>> input){
+    m_vectors.resize(input.size());
+    m_vectors[0].resize(input[0].size());
+    applyKVectorOperation(input, add_op);
+}
+
+//////////////////////////////////////////////////////////
+
+void KVector::operator=(const KVector& v) {
+    m_vectors.resize(v.m_vectors.size());
+    m_vectors[0].resize(v.m_vectors[0].size());
+    applyKVectorOperation(v.m_vectors, add_op);
+}
+
+void KVector::operator+=(const KVector& v) {
+    applyKVectorOperation(v.m_vectors, add_op);
+}
+
+void KVector::operator-=(const KVector& v) {
+    applyKVectorOperation(v.m_vectors, sub_op);
+}
+
+void KVector::operator*=(const KVector& v) {
+    applyKVectorOperation(v.m_vectors, mult_op);
+}
+
+void KVector::operator/=(const KVector& v) {
+    applyKVectorOperation(v.m_vectors, dev_op);
+}
+
+//////////////////////////////////////////////////////////
+
+KVector KVector::operator+(const KVector& v) {
+    KVector temp {*this};
+    temp.applyKVectorOperation(v.m_vectors, add_op);
+    return temp;
+}
+
+KVector KVector::operator-(const KVector& v) {
+    KVector temp {*this};
+    temp.applyKVectorOperation(v.m_vectors, sub_op);
+    return temp;
+}
+
+KVector KVector::operator*(const KVector& v) {
+    KVector temp {*this};
+    temp.applyKVectorOperation(v.m_vectors, mult_op);
+    return temp;
+}
+
+KVector KVector::operator/(const KVector& v) {
+    KVector temp {*this};
+    temp.applyKVectorOperation(v.m_vectors, dev_op);
+    return temp;
+}
+
+//////////////////////////////////////////////////////////
+
+KVector KVector::operator+(const std::vector<float>& v){
+    KVector temp {*this};
+    temp.applyVectorOperation(v, add_op);
+    return temp;
+}
+
+KVector KVector::operator-(const std::vector<float>& v){
+    KVector temp {*this};
+    temp.applyVectorOperation(v, sub_op);
+    return temp;
+}
+
+KVector KVector::operator*(const std::vector<float>& v){
+    KVector temp {*this};
+    temp.applyVectorOperation(v, mult_op);
+    return temp;
+}
+
+KVector KVector::operator/(const std::vector<float>& v){
+    KVector temp {*this};
+    temp.applyVectorOperation(v, dev_op);
+    return temp;
+}
+
+//////////////////////////////////////////////////////////
+
+KVector KVector::operator+(double add){
+    KVector temp {*this};
+    temp.applyScalarOperation(add, mult_op);
+    return temp;
+}
+
+KVector KVector::operator-(double sub){
+    KVector temp {*this};
+    temp.applyScalarOperation(sub, mult_op);
+    return temp;
+}
+
+KVector KVector::operator*(double scale){
+    KVector temp {*this};
+    temp.applyScalarOperation(scale, mult_op);
+    return temp;
+}
+
+KVector KVector::operator/(double dev){
+    KVector temp {*this};
+    temp.applyScalarOperation(dev, mult_op);
+    return temp;
+}
+
+//////////////////////////////////////////////////////////
+
+KVector KVector::setConstant(double value){
+    applyScalarOperation(0, set_op);
+    applyScalarOperation(value, add_op);
+    return *this;
+}
+
+KVector KVector::setConstant(std::vector<double>& vector){
+    applyScalarOperation(0, set_op);
+    applyScalarOperation(vector, add_op);
+    return *this;
+}
+const double KVector::magnitude(int index){
+    if (index < 0 || index >= m_vectors.size()) {
+        return -1;
+    }
+    double sum = 0;
+    for (int i = 0; i < m_vectors.size(); ++i) {
+        sum += m_vectors[index][i] * m_vectors[index][i];
+    }
+    return std::sqrt(sum);
+
+}
+
+const double KVector::maxMagnitude(){
+    double max {};
+    for (int i = 0; i < m_vectors.size(); ++i){
+       max = std::max(max, magnitude(i));
+    }
+    return max;
+}
+
+const double KVector::avgMagnitude(){
+    double max {};
+    for (int i = 0; i < m_vectors.size(); ++i){
+       max = std::max(max, magnitude(i));
+    }
+    return max/m_vectors.size();
+
+}
